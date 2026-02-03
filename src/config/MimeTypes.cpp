876b669b6d05691a0eae90f55277639da1120cb1
OmarIssa0 <omar.issa.slwade@gmail.com>
2026-02-03 21:50:15 +0300

    fix: add missing MIME types for txt, xml, json, svg, webp, woff, woff2, ttf, and pdf

--- src/config/MimeTypes.cpp
+++ src/config/MimeTypes.cpp
@@ -5,18 +5,34 @@ MimeTypes::MimeTypes(const std::string& extension) : extension(extension) {
         mimeType = "text/html";
     else if (extension == "css")
         mimeType = "text/css";
+    else if (extension == "txt")
+        mimeType = "text/plain";
+    else if (extension == "xml")
+        mimeType = "text/xml";
     else if (extension == "js")
         mimeType = "application/javascript";
+    else if (extension == "json")
+        mimeType = "application/json";
     else if (extension == "png")
         mimeType = "image/png";
     else if (extension == "jpg" || extension == "jpeg")
         mimeType = "image/jpeg";
     else if (extension == "gif")
         mimeType = "image/gif";
-    else if (extension == "txt")
-        mimeType = "text/plain";
     else if (extension == "ico")
         mimeType = "image/x-icon";
+    else if (extension == "svg")
+        mimeType = "image/svg+xml";
+    else if (extension == "webp")
+        mimeType = "image/webp";
+    else if (extension == "woff")
+        mimeType = "font/woff";
+    else if (extension == "woff2")
+        mimeType = "font/woff2";
+    else if (extension == "ttf")
+        mimeType = "font/ttf";
+    else if (extension == "pdf")
+        mimeType = "application/pdf";
     else
         mimeType = "application/octet-stream";
 }
